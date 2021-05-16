f41ed93dbe0a8a1ee87cffb498fae41a7873b232
ManiaDaneshfar <maniadaneshfar@outlook.com>
2021-05-16 23:43:22 +0430

    initGetForCreate

--- Spice/Areas/Admin/Controllers/SubCategoryController.cs
+++ Spice/Areas/Admin/Controllers/SubCategoryController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Spice.Data;
+using Spice.Models;
+using Spice.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,11 +19,26 @@ namespace Spice.Areas.Admin.Controllers
         {
             _db = db;
         }
+        //GET   INDEX
         public async Task< IActionResult> Index()
         {
             var subCategories = await _db.SubCategory.Include(s=>s.Category).ToListAsync();
            
             return View(subCategories);
+        }
+
+
+        //GET Create
+        public async Task<IActionResult> Create()
+        {
+            SubCategoryAndCategoryViewModel model = new SubCategoryAndCategoryViewModel()
+            {
+                CategoryList = await _db.Category.ToListAsync(),
+                SubCategory = new Models.SubCategory(),
+                SubCategoryList = await _db.SubCategory.OrderBy(p => p.Name).Select(p => p.Name).Distinct().ToListAsync()
+
+            };
+            return View(model);
         }
     }
 }
