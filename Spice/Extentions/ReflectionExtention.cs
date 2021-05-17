b31654e52998bdc8920ffeb0f774ec33931d8bad
ManiaDaneshfar <maniadaneshfar@outlook.com>
2021-05-17 13:05:51 +0430

    initExtentionMethod

---
+++ Spice/Extentions/ReflectionExtention.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spice.Extentions
+{
+    public static class ReflectionExtention
+    {
+        public static string GetPropertyValue<T>(this T item, string propertyName)
+        {
+            return item.GetType().GetProperty(propertyName).GetValue(item, null).ToString();
+
+        }
+    }
+}
