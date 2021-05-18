cf8d54f799725269e5b02fbb164bd7817bfc28ef
ManiaDaneshfar <maniadaneshfar@outlook.com>
2021-05-18 19:59:45 +0430

    initSubCategoryEdieAction

--- Spice/Areas/Admin/Controllers/SubCategoryController.cs
+++ Spice/Areas/Admin/Controllers/SubCategoryController.cs
@@ -45,6 +45,7 @@ namespace Spice.Areas.Admin.Controllers
             };
             return View(model);
         }
+        //POST    CREATE
 
         [HttpPost]
         [ValidateAntiForgeryToken]
@@ -86,6 +87,63 @@ namespace Spice.Areas.Admin.Controllers
                              where subCategory.CategoryId == id
                              select subCategory).ToListAsync();
             return Json(new SelectList(subCategories,"Id","Name"));
+        }
+
+
+
+        //GET EDIT
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var subCategory = await _db.SubCategory.SingleOrDefaultAsync(m=>m.Id==id);
+            if (subCategory == null)
+            {
+                return NotFound();
+            }
+            SubCategoryAndCategoryViewModel model = new SubCategoryAndCategoryViewModel()
+            {
+                CategoryList = await _db.Category.ToListAsync(),
+                SubCategory = subCategory,
+                SubCategoryList = await _db.SubCategory.OrderBy(p => p.Name).Select(p => p.Name).Distinct().ToListAsync()
+
+            };
+            return View(model);
+        }
+        //POST    EDIT
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id,SubCategoryAndCategoryViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var doesSubCategoryExists = _db.SubCategory.Include(s => s.Category).Where(s => s.Name == model.SubCategory.Name && s.Category.Id == model.SubCategory.CategoryId);
+
+                if (doesSubCategoryExists.Count() > 0)
+                {
+                    //Error
+                    StatusMessage = "Error: Sub Category Exists Under" + doesSubCategoryExists.First().Category.Name + "Category.Please Use Another name.";
+                }
+                else
+                {
+                    _db.SubCategory.Add(model.SubCategory);
+                    await _db.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            SubCategoryAndCategoryViewModel modelVM = new SubCategoryAndCategoryViewModel()
+            {
+                CategoryList = await _db.Category.ToListAsync(),
+                SubCategory = new Models.SubCategory(),
+                SubCategoryList = await _db.SubCategory.OrderBy(p => p.Name).Select(p => p.Name).Distinct().ToListAsync(),
+                StatusMessage = StatusMessage
+
+            };
+            return View(modelVM);
+
         }
     }
 }
