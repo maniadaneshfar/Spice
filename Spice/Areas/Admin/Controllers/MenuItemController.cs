9b98f693c91830097d848aaa5e33252f16536f3c
ManiaDaneshfar <maniadaneshfar@outlook.com>
2021-06-06 00:36:17 +0430

    initMenuItemViewModelandCreateGET

--- Spice/Areas/Admin/Controllers/MenuItemController.cs
+++ Spice/Areas/Admin/Controllers/MenuItemController.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Hosting.Internal;
 using Spice.Data;
+using Spice.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,15 +16,27 @@ namespace Spice.Areas.Admin.Controllers
     {
         private readonly ApplicationDbContext _db;
         private readonly IHostingEnvironment _hostingEnviornment;
+        [BindProperty]
+        public MenuItemViewModel MenuItemVM { get; set; }
         public MenuItemController(ApplicationDbContext db, IHostingEnvironment hostingEnviornment)
         {
             _db = db;
             _hostingEnviornment = hostingEnviornment;
+            MenuItemVM = new MenuItemViewModel()
+            {
+                Category = _db.Category,
+                MenuItem = new Models.MenuItem()
+            };
         }
         public async Task <IActionResult> Index()
         {
             var menuItems = await _db.MenuItem.Include(m=>m.Category).Include(m=>m.SubCategory).ToListAsync();
             return View(menuItems);
+        }
+        //GET-Creat
+        public IActionResult Create()
+        {
+            return View(MenuItemVM);
         }
     }
 }
